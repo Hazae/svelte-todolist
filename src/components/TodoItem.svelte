6893b8358cabe426bc08f5cf73dc23ca101d9dbb
s2ryu1 <xenn23@kakao.com>
2021-10-14 17:00:39 +0900

    todo list, item

---
+++ src/components/TodoItem.svelte
@@ -0,0 +1,46 @@
+<script>
+  export let todo;
+
+  let placeholder = "할 일을 입력해주세요";
+</script>
+
+<style lang="scss">
+  .control.has-icons-left,
+  .control.has-icons-right{
+    & .icon{
+      pointer-events: inherit;
+      transition: color 0.5s;
+      cursor: pointer;
+
+      &:hover{
+        color: #363636;
+      }
+    }
+    & .input:focus ~ .icon{
+      color: #dbdbdb;
+    }
+  }
+  .todo-item{
+    &--checked{
+      opacity: 0.4;
+    }
+  }
+</style>
+
+<div class="field">
+  <div
+    class="control has-icons-left has-icons-right todo-item"
+    class:todo-item--checked={todo.checked}>
+    <input
+      class="input"
+      type="text"
+      {placeholder}
+      bind:value={todo.text} />
+    <span class="icon is-small is-left">
+      <i class="fas fa-check"></i>
+    </span>
+    <span class="icon is-small is-right">
+      <i class="fas fa-trash"></i>
+    </span>
+  </div>
+</div>
---
+++ src/components/TodoList.svelte
@@ -0,0 +1,19 @@
+<script>
+  import TodoItem from "./TodoItem.svelte";
+
+  export let todos;
+
+  $: remaining = todos.filter((t) => !t.checked).length;
+</script>
+
+<div>
+  {#if remaining}
+    <p>남은 일이 {remaining}개 남았어요</p>
+  {:else}
+    <p>남은 일이 없어요</p>
+  {/if}
+  <br>
+  {#each todos as todo (todo.id)}
+    <TodoItem {todo} />
+  {/each}
+</div>
